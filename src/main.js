c60dd25fbd0a1da98c6634136ffc2d2e61f94866
robin_time <robin_time@163.com>
2019-08-11 18:08:24 +0800

    前端页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import '@/styles/index.scss' // global css
+
 import App from './App'
+import store from './store'
 import router from './router'
+import axios from 'axios'
+
+import '@/icons' // icon
+
+axios.defaults.baseURL = 'http://127.0.0.1:9000/api/'
+Vue.prototype.$axios = axios
+
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
@@ -10,6 +26,14 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
+
+// axios({
+//   url: 'http://127.0.0.1:9000/api/user/login',
+//   method: 'post'
+// }).then(res => {
+//   console.log(res)
+// })
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,53 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-export default new Router({
-  // 去掉url中的#
-  mode: 'history',
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+// export default new Router({
+//   // 去掉url中的#
+//   mode: 'history',
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     },
+//     {
+//       path: '/login',
+//       component: () => import('@/views/login/index'),
+//       hidden: true
+//     }
+//   ]
+// })
+
+export const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index')
+  },
+
+  {
+    path: '/404',
+    component: () => import('@/views/404'),
+    hidden: true
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', redirect: '/404', hidden: true }
+]
+
+const createRouter = () => new Router({
+  mode: 'history', // require service support
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRoutes
 })
+
+const router = createRouter()
+
+// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher // reset router
+}
+
+export default router
